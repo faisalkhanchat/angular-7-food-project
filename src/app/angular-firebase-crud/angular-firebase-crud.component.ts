c9873a52aeff6ec96a35f36b44a22bfa71dabe86
Faisal Khan <faisal.khan@appinventiv.com>
2020-10-01 10:06:37 +0530

    ng-curd

---
+++ src/app/angular-firebase-crud/angular-firebase-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-firebase-crud',
+  templateUrl: './angular-firebase-crud.component.html',
+  styleUrls: ['./angular-firebase-crud.component.scss']
+})
+export class AngularFirebaseCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/angular-firebase-crud/angular-firebase-crud.module.ts
+++ src/app/angular-firebase-crud/angular-firebase-crud.module.ts
@@ -1,12 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularFirebaseCrudComponent } from './angular-firebase-crud.component';
 
-
+const inrRoute: Routes = [
+  { path: '', component: AngularFirebaseCrudComponent }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [AngularFirebaseCrudComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(inrRoute)
   ]
 })
 export class AngularFirebaseCrudModule { }
